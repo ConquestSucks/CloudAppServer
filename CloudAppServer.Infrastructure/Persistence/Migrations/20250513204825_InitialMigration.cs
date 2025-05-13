a7aac54953b3e4f767422d29aacdf3e1a94fbd5b
horex <nikita.horex@icloud.com>
2025-05-14 02:19:01 +0500

    Added initial migration

---
+++ CloudAppServer.Infrastructure/Persistence/Migrations/20250513204825_InitialMigration.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudApp.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    FreeDiskSpace = table.Column<decimal>(type: "numeric", nullable: false),
+                    DiskSpaceLeft = table.Column<decimal>(type: "numeric", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CloudFolders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ParentCloudFolderId = table.Column<Guid>(type: "uuid", nullable: true),
+                    PublicUrl = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CloudFolders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CloudFolders_CloudFolders_ParentCloudFolderId",
+                        column: x => x.ParentCloudFolderId,
+                        principalTable: "CloudFolders",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_CloudFolders_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CloudFiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CloudFolderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PublicUrl = table.Column<string>(type: "text", nullable: true),
+                    Size = table.Column<decimal>(type: "numeric", nullable: false),
+                    Extension = table.Column<string>(type: "text", nullable: false),
+                    DownloadCount = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CloudFiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CloudFiles_CloudFolders_CloudFolderId",
+                        column: x => x.CloudFolderId,
+                        principalTable: "CloudFolders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CloudFiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CloudFiles_CloudFolderId",
+                table: "CloudFiles",
+                column: "CloudFolderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CloudFiles_UserId",
+                table: "CloudFiles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CloudFolders_ParentCloudFolderId",
+                table: "CloudFolders",
+                column: "ParentCloudFolderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CloudFolders_UserId",
+                table: "CloudFolders",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CloudFiles");
+
+            migrationBuilder.DropTable(
+                name: "CloudFolders");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
