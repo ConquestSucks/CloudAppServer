041e7337f4cba275c8392c78959ae97005f9f24d
horex <nikita.horex@icloud.com>
2025-05-14 02:17:29 +0500

    Added BaseEntity for Domain Entities

---
+++ CloudAppServer.SharedKernel/Abstractions/BaseEntity.cs
@@ -0,0 +1,16 @@
+namespace CloudApp.SharedKernel.Abstractions;
+
+public class BaseEntity
+{
+    public Guid Id { get; set; }
+    
+    public required string Name { get; set; }
+    
+    public bool IsDeleted { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public DateTime ModifiedAt { get; set; }
+    
+    public DateTime? DeletedAt { get; set; }
+}
